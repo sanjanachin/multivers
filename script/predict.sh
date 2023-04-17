99eaefb3b82c79bb7f931070804343422a9383f4
sanjanachintalapati <sanjanac@cs.washington.edu>
2023-04-17 13:15:37 -0700

    updated predict.sh for domains 2 and 3

--- script/predict.sh
+++ script/predict.sh
@@ -15,12 +15,26 @@ function predict_rob_scifact_exact_sentences() {
         --corpus_file=data/rob_data/sg/corpus.jsonl \
         --output_file=prediction/rob_data_exact_sentences.jsonl
 }
-function predict_rob_scifact_new_claims() {
+function predict_rob_d1_scifact_new_claims() {
     python multivers/predict.py \
         --checkpoint_path=checkpoints/scifact.ckpt \
         --input_file=data/rob_data/sg/claims_test_retrieved_new_claims.jsonl \
         --corpus_file=data/rob_data/sg/corpus.jsonl \
         --output_file=prediction/rob_data_new_claims.jsonl
+}
+function predict_rob_d2_scifact() {
+    python multivers/predict.py \
+        --checkpoint_path=checkpoints/scifact.ckpt \
+        --input_file=data/rob_data/ac_domain_2/claims_test_retrieved.jsonl \
+        --corpus_file=data/rob_data/ac_domain_2/corpus.jsonl \
+        --output_file=prediction/ac_domain_2.jsonl
+}
+function predict_rob_d3_scifact() {
+    python multivers/predict.py \
+        --checkpoint_path=checkpoints/scifact.ckpt \
+        --input_file=data/rob_data/incomplete_domain_3/claims_test_retrieved.jsonl \
+        --corpus_file=data/rob_data/incomplete_domain_3/corpus.jsonl \
+        --output_file=prediction/incomplete_domain_3.jsonl
 }
 function predict_scifact() {
     python multivers/predict.py \
@@ -61,9 +75,15 @@ then
 elif [[ $model == "rob_scifact_exact_sentences" ]]
 then
     predict_rob_scifact_exact_sentences
-elif [[ $model == "rob_scifact_new_claims" ]]
+elif [[ $model == "rob_d1_scifact_new_claims" ]]
 then
     predict_rob_scifact_new_claims
+elif [[ $model == "rob_d2_scifact" ]]
+then
+    predict_rob_d2_scifact
+elif [[ $model == "rob_d3_scifact" ]]
+then
+    predict_rob_d3_scifact
 elif [[ $model == "scifact" ]]
 then
     predict_scifact
